b8179998d98a9d4e787df92a3218fc7919b705c4
Lonis <164430029+lonis752@users.noreply.github.com>
2024-09-10 00:31:51 -0700

    getting all the buttons linked in javascript

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const clear = document.getElementById("clear");
+const sign = document.getElementById("sign");
+const percent = document.getElementById("percent");
+const divide = document.getElementById("/");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+const multiply = document.getElementById("*");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const subtract = document.getElementById("-");
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const addition = document.getElementById("+");
+const zero = document.getElementById("0");
+const decimal = document.querySelector(".decimal");
+const equals = document.querySelector(".equals");
+const display = document.getElementById("display");
+
+const numbers = () => {
+    if (optr === "") {
+        num1 += 
+    }
+}
+
+one.addEventListener("click", )
+
+let num1 = "";
+let optr = "";
+let num2 = "";
